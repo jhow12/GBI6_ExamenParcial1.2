665c76b9c7f5ed7b2147c345999f9f4de7dc59dd
Joha.Pa <johanna.paez@est.ikiam.edu.ec>
2022-01-15 02:39:41 -0500

    Paez Johanna ha respondido el ejercicio 2

---
+++ pregunta2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#El comando para imprimir el directorio en el que nos encontramos es
+pwd
+ #debido a que nos encontramos en el directorio principal no da 
+#como resultado
+/c/Users/Clienta
+ 
+
